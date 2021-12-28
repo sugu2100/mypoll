e48669c77a583144436b2c2907846765623a6d8c
sugu2100 <sugu2100@daum.net>
2021-12-28 20:19:58 +0900

    host change

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2)ma5*v(*65tit!_u+s5+iad^k1#lb^t@5c_3d$_r4sj1q+int'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
